2cf400fbd68cacc3203f3ca18a3ab27b8c8b902c
gilandk <gilandk@gmail.com>
2021-02-19 10:39:49 +0800

    Delete item

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   get 'item/:id', to: 'items#view', as: 'item', id: /\d+/
   get 'item/add', to: 'items#add', as: 'add_item'
   get 'item/:id/edit', to: 'items#edit', as: 'edit_item'
+ 
   patch 'item/:id', to: 'items#update'
-  
   post 'items', to: 'items#create'
+  delete 'items/:id', to: 'items#destroy', as: 'delete_item'
 
   get 'users/list'
   get 'users/add'
