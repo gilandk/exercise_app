8ae81136c3141c834c83c3f163733a0a67857810
gilandk <gilandk@gmail.com>
2021-02-19 15:29:18 +0800

    Update controllers

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
-    belongs_to :user
+    belongs_to :user, :optional => true
+
     validates :name, length: { maximum: 50 }, presence: true
     validates :quantity, presence: true
     validates :price, presence: true
-    validates :userid, presence: true
 end
+    
