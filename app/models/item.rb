9cf98ccd5a2c4679fa6b0562632276ac2eb30c83
gilandk <gilandk@gmail.com>
2021-02-19 14:54:23 +0800

    Changes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
     belongs_to :user
-    validates :name, length: { maximum: 10 }, presence: true
+    validates :name, length: { maximum: 50 }, presence: true
     validates :quantity, presence: true
     validates :price, presence: true
     validates :userid, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :items
-    validates :name, length: { maximum: 10 }, presence: true
+    validates :name, length: { maximum: 50 }, presence: true
     validates :email, presence: true
 
 
